43d6fefcbc45b76e5eda3b7f8938816ef0f89b83
Prabhjeet <prabhjeetsingh054@gmail.com>
2026-04-24 18:45:08 +0530

    configured cors properly

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,8 @@ const ALLOWED_ORIGINS = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://localhost:5173",
-  "https://127.0.0.1:5173"
+  "https://127.0.0.1:5173",
+  "bajaj-f-inserv.vercel.app"
 ];
 
 app.use(
